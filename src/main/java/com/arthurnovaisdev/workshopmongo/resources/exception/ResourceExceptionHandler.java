95df25021750c5d2fd6341b52d3036f495b0ca34
arthurnovaisdev <arthurnovaispeixoto@gmail.com>
2026-09-13 16:23:58 -0300

    getting user by ID

---
+++ src/main/java/com/arthurnovaisdev/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.arthurnovaisdev.workshopmongo.resources.exception;
+
+import com.arthurnovaisdev.workshopmongo.services.exception.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandartError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandartError err = new StandartError(System.currentTimeMillis(), status.value(), "Não encontrado.", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
