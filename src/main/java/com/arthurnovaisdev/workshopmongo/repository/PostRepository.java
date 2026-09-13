9f5b63d849fd713ad0fb620a87e98cc6b5750d61
arthurnovaisdev <arthurnovaispeixoto@gmail.com>
2026-09-13 18:22:40 -0300

    creating entity with nested User

---
+++ src/main/java/com/arthurnovaisdev/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,14 @@
+package com.arthurnovaisdev.workshopmongo.repository;
+
+import com.arthurnovaisdev.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
